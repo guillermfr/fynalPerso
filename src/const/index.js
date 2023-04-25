a1df317e9787f5c18592e9998bcc7bb5cc7c00dc
François <81768287+guillermfr@users.noreply.github.com>
2023-04-25 15:39:52 +0200

    Delete src directory

--- src/const/index.js
+++
@@ -1,2 +0,0 @@
-export * from './filtre'
-export * from './profile'
--- src/fonctions/prixReductionArrondi.js
+++
@@ -1,3 +0,0 @@
-export function prixReductionArrondi(prix, reduction) {
-    return Math.round(((prix - (reduction/100)*prix) + Number.EPSILON) * 100) / 100
-}
